a76a571137d99efbbad99f8d70401e352792d6b4
vidhidhakan <158539298+vidhidhakan@users.noreply.github.com>
2024-08-21 11:46:22 -0400

    Update commands.bash

--- git_related/commands.bash
+++ git_related/commands.bash
@@ -26,7 +26,7 @@ cd sql_coding_01//
 
 git add .
 git commit -am "changes saved for sqls"  -> Saves
-git push - to forward this file in repo .....
+git push - to forward this file in repo .........
 
 =======================================================================================================
 
